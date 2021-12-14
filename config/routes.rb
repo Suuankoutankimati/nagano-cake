442078bb84b675f48de08994cb873252125ab5a6
EC2 Default User <s.marika441@gmail.com>
2021-12-14 02:13:55 +0000

    [Add]デバイス機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #管理者
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+  #顧客用
+  devise_for :customers,skip: [:passwords,], controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
 end
