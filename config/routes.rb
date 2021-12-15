b4a9087269fa05be1353e80fa7cb4e0bbc96cbdd
Yakaaaan11 <ayasax.0611@gmail.com>
2021-12-15 09:19:29 +0000

    [Add]ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+
+  
   #管理者
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
@@ -9,13 +11,28 @@ Rails.application.routes.draw do
     registrations: "customer/registrations",
     sessions: 'customer/sessions'
   }
-
+  
+  # 顧客側
   scope module: :customer do
     resources :orders,only: [:index,:show,:new,:create] do
-        collection do
-          post 'confirm'
-          get 'completion'
-        end
+      collection do
+        post 'confirm'
+        get 'completion'
       end
+    end
+    
+    root "homes#top"
+    get "about" => "homes#about"
+    
+    resources :customers,only: [:show,:edit,:update] do
+      collection do
+        get "unsubscribe"
+        patch "withdraw"
+      end
+    end
+    
+    resources :addresses,only: [:index,:edit] do
+    end
   end
+  
 end
