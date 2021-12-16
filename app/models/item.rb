c4d9bad046e4cd0685a3124873137da23cadc344
Atsushi1216 <ats1216@gmail.com>
2021-12-16 14:18:20 +0000

    [Add]商品一覧

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
-    belongs_to :genre, optional: true
+
+    belongs_to :genre,foreign_key: 'genre_id',optional: true
     attachment :image
+
+    validates :name, presence: true
+    validates :non_tax_price, presence: true
+	validates :introduction, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'homes#top'
+
   namespace :admin do
     resources :items
     resources :genres
+    root 'homes#top'
   end
-  
+
   namespace :customer do
     get 'cart_items/index'
   end
@@ -18,7 +19,7 @@ Rails.application.routes.draw do
     registrations: "customer/registrations",
     sessions: 'customer/sessions'
   }
-  
+
   # 顧客側
   scope module: :customer do
     resources :orders,only: [:index,:show,:new,:create] do
@@ -27,27 +28,27 @@ Rails.application.routes.draw do
         get 'completion'
       end
     end
-    
+
     root "homes#top"
     get "about" => "homes#about"
-    
+
     resources :customers,only: [:show,:edit,:update] do
       collection do
         get "unsubscribe"
         patch "withdraw"
       end
     end
-    
+
     resources :addresses,only: [:index,:edit,:create,:update,:destroy] do
     end
-    
+
     resources :cart_items,only:[:index,:update,:create,:destroy] do
       collection do
         delete "destroy_all"
       end
     end
-    
+
     resources :items,only: [:index,:show]
   end
-  
+
 end
