3400a537899ba8e851ff8e1fd355e7ba4876e4ba
GenkiWatanabe <87759603+Genki7@users.noreply.github.com>
2021-12-16 08:23:37 +0000

    [Add]注文実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
-  root 'homes#top'
+
   namespace :admin do
+    root "homes#top"
     resources :items
     resources :genres
+    resources :orders,only: [:index,:show]
+    resources :customers
+    
   end
-  
+
   namespace :customer do
     get 'cart_items/index'
   end
@@ -18,7 +22,7 @@ Rails.application.routes.draw do
     registrations: "customer/registrations",
     sessions: 'customer/sessions'
   }
-  
+
   # 顧客側
   scope module: :customer do
     resources :orders,only: [:index,:show,:new,:create] do
@@ -27,27 +31,27 @@ Rails.application.routes.draw do
         get 'completion'
       end
     end
-    
+
     root "homes#top"
     get "about" => "homes#about"
-    
+
     resources :customers,only: [:show,:edit,:update] do
       collection do
         get "unsubscribe"
         patch "withdraw"
       end
     end
-    
+
     resources :addresses,only: [:index,:edit,:create,:update,:destroy] do
     end
-    
+
     resources :cart_items,only:[:index,:update,:create,:destroy] do
       collection do
         delete "destroy_all"
       end
     end
-    
+
     resources :items,only: [:index,:show]
   end
-  
+
 end
