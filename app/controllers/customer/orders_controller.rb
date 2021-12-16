cdacd4057937d632a96ee68a340cb537238fa384
EC2 Default User <s.marika441@gmail.com>
2021-12-16 02:27:39 +0000

    [Add]注文確認画面の追加

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -5,9 +5,23 @@ class Customer::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    @order.post_code = current_customer.post_code
-    @order.address = current_customer.address
-    @order.name = current_customer.last_name + current_customer.first_name
+    # address_numberの値が"1"のとき
+    if params[:order][:address_number] == "1"
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    # address_numberの値が"2"のとき
+    elsif params[:order][:address_number] == "2"
+      @address = Address.find(params[:order][:address_id])
+      @order.post_code = @address.post_code
+      @order.address = @address.address
+      @order.name = @address.name
+    # address_numberの値が"3"のとき
+    elsif params[:order][:address_number] == "3"
+      @order.post_code = params[:order][:post_code]
+      @order.address     = params[:order][:address]
+      @order.name        = params[:order][:name]
+    end
   end
 
   private
