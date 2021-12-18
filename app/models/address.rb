7604bcca24a4186ee14f1fdf0809af0a0657e46a
Yakaaaan11 <ayasax.0611@gmail.com>
2021-12-18 06:22:01 +0000

    [Update]カート合計金額、アドレス・顧客のバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,9 @@ class Address < ApplicationRecord
   def full_address
     '〒' + post_code + ' ' + address + ' ' + name
   end
+  
+  VALID_POSTAL_CODE_REGEX = /\A\d{7}\z/
+  validates :post_code, presence: true, format: {with:VALID_POSTAL_CODE_REGEX}
+  validates :address, presence: true
+  validates :name, presence: true
 end
