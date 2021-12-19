7d533e67e1ccf0800f23e889aa37128f81ed49b1
EC2 Default User <s.marika441@gmail.com>
2021-12-19 02:10:58 +0000

    [Add]カート商品を注文詳細に保存

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,10 @@
 class Order < ApplicationRecord
 
-    belongs_to :customer
+  belongs_to :customer
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { wait: 0, confirm: 1, production: 2,preparation: 3,completion: 4 }
+
+  attribute :shipping_cost, default: 800
 
 end
