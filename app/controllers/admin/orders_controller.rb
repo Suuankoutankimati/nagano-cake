6f3254c707beae745e13c0a6d939ca31f98f9f78
GenkiWatanabe <87759603+Genki7@users.noreply.github.com>
2021-12-22 08:59:20 +0000

    [Fix]order詳細画面修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -25,7 +25,7 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @ordered = OrderDetail.where(id: @order.id)
+    @ordered = @order.order_details
   end
 
   def update
