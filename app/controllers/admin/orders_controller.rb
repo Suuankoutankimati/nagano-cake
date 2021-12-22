5979b4405b42d11e9693a2e3fe36e3c09bd756b0
GenkiWatanabe <87759603+Genki7@users.noreply.github.com>
2021-12-22 14:23:37 +0000

    [Add]admin要件定義終了

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,10 +5,10 @@ class Admin::OrdersController < ApplicationController
   def index
     @count = Order.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count
 
-    # 雷
+    # ダイレクト先によるpathの取得の条件分岐
+    
     # before_uri = URI.parse(request.referer)
     # @path = Rails.application.routes.recognize_path(before_uri.path)
-
     # if @path[:action] == "top"
     #   @orders = Order.page(params[:page]).where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
     # elsif @path[:action] == "show"
@@ -20,7 +20,6 @@ class Admin::OrdersController < ApplicationController
 
     @orders = Order.page(params[:page]).reverse_order
     @ordered = OrderDetail.where(params[:id])
-    #binding.pry
   end
 
   def show
@@ -31,10 +30,10 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @ordered = @order.order_details
+    #注文ステータス製造ステータスの連動　enum（１→２）
     @order.update(order_params)
     if @order.status == "confirm"
       @ordered.each do |ordered|
-         #binding.pry
         ordered.update(making_status: "wait")
       end
     end
@@ -48,8 +47,8 @@ class Admin::OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:status)
   end
-  
-  
+
+
   # def if_not_admin
   #   redirect_to new_admin_session_path unless admin_signed_in?
   # end
