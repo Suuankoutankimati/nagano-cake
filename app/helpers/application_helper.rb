a66bf42b7d16f1bcc207cab62b311a5072db8509
EC2 Default User <s.marika441@gmail.com>
2021-12-23 02:12:00 +0000

    [Add]注文履歴一覧にsort機能追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-
-  # sort機能のヘルパーメソッド
-  def sort_order(column, created_at)
-      css_class = column == sort_column ? "current #{sort_direction}" : nil
-      direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-      link_to orders, { sort: column, direction: direction }, class: "sort_header #{css_class}"
-  end
-
 end
